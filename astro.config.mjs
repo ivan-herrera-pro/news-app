86beb5eea185be00b6b7854b7b873fc062905fc8
Iván Andrés Herrera Pertuz <106353153+ivan-herrera-pro@users.noreply.github.com>
2024-01-19 12:01:02 -0500

    feat: add vercel config to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://ivan-herrera-pro.github.io',
   base: '/news-app',
+  output: 'server',
+  adapter: vercel(),
 });
